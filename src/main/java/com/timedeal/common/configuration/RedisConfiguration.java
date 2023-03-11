4dc3054655a0e36050a806308aede0ad75d35443
drow724 <drow724@naver.com>
2023-03-11 10:04:53 +0900

    change redis coufiguration

--- src/main/java/com/timedeal/common/configuration/RedisConfiguration.java
+++ src/main/java/com/timedeal/common/configuration/RedisConfiguration.java
@@ -22,10 +22,10 @@ import redis.embedded.RedisServer;
 @EnableRedisHttpSession
 public class RedisConfiguration {
 
-	@Value("${spring.redis.port}")
+	@Value("${spring.data.redis.port}")
 	private int port;
 
-	@Value("${spring.redis.host}")
+	@Value("${spring.data.redis.host}")
 	private String host;
 
 	@Bean
