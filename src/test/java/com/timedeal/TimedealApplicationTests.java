2611b94253ec2d8d820876ec1e4c52cd1eccf313
drow724 <drow724@naver.com>
2023-03-16 14:00:12 +0900

    add test properties

--- src/test/java/com/timedeal/TimedealApplicationTests.java
+++ src/test/java/com/timedeal/TimedealApplicationTests.java
@@ -2,7 +2,9 @@ package com.timedeal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TimedealApplicationTests {
 
